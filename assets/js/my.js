1fdbc9a0e7c811b919154e372799374833324c46
greenshark-ben <benjamin@greenshark.com>
2018-08-31 15:33:07 -0500

    primer commit :cat:

---
+++ assets/js/my.js
@@ -0,0 +1,7 @@
+// var element = document.getElementById('parallax');
+
+// document.body.onscroll = function() {
+//   if(window.scrollY < element.scrollHeight) {
+//     element.style.top = (window.scrollY / element.scrollHeight *  100) / 8 + '%';
+//   }
+// }
